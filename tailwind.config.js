e9c1b5e9b3b02bb575582453f9b02a8fc4ac13c8
Michael van Rooijen <michael@vanrooijen.io>
2022-09-07 19:44:23 +0200

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,10 @@
-// const plugin = require('tailwindcss/plugin')
-
-// module.exports = {
-//   plugins: [
-//     plugin(function({ addVariant }) {
-//       addVariant('hocus', ['&:hover', '&:focus'])
-//     })
-//   ]
-// }
-
-
 module.exports = {
   content: ["./source/**/*.html.erb"],
 
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
   ]
 }
